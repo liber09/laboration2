02c51e86685655491ec4efcbbe399504444bad06
liber09 <linda.bergsangel@hotmail.com>
2023-11-23 03:44:30 +0100

    map place model to placeDto

--- src/main/java/com/example/labb2/dto/PlaceDto.java
+++ src/main/java/com/example/labb2/dto/PlaceDto.java
@@ -1,5 +1,33 @@
 package com.example.labb2.dto;
 
-public class PlaceDto {
+import com.example.labb2.model.Category;
+import com.example.labb2.model.User;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import lombok.experimental.Accessors;
+import org.springframework.data.geo.Point;
 
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@Accessors(chain = true)
+@NoArgsConstructor
+@ToString
+@JsonInclude(value = JsonInclude.Include.NON_NULL)
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class PlaceDto {
+    private Long placeId;
+    private String name;
+    private Category category;
+    private User user;
+    private String status;
+    private LocalDate created;
+    private LocalDate lastUpdated;
+    private String description;
+    private Point coordinates;
 }
---
+++ src/main/java/com/example/labb2/dto/mapper/PlaceMapper.java
@@ -0,0 +1,18 @@
+package com.example.labb2.dto.mapper;
+
+import com.example.labb2.dto.PlaceDto;
+import com.example.labb2.model.Place;
+
+public class PlaceMapper {
+    public static PlaceDto toPlaceDto(Place place){
+        return new PlaceDto()
+                .setPlaceId(place.getPlaceId())
+                .setCategory(place.getCategory())
+                .setCoordinates(place.getCoordinates())
+                .setName(place.getName())
+                .setCreated(place.getCreated())
+                .setDescription(place.getDescription())
+                .setStatus(place.getStatus())
+                .setUser(place.getUser());
+    }
+}
